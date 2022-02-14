13e0afdf3b27ea8fec94cd24342d2934fdb74c7c
Patiljyoti12 <59950788+Patiljyoti12@users.noreply.github.com>
2022-02-14 16:47:14 +0530

    Update check_limits.py

--- check_limits.py
+++ check_limits.py
@@ -1,5 +1,38 @@
-def print_warning(message):
-    print("{} is out of range!".format(message))
+#Temperature_tolerance
+def check_temp_tolerance(temperature):
+    if (temperature<0 and  temperature>45):
+        temp_message="Temperature out of range"
+    elif (temperature>=0 and temperature<=(0+0.05*45)):
+        temp_message="Warning:Temperature Approaching low"
+    elif (temperature >=(45-0.05*45) and temperature<=45):
+        temp_message="Warning:Temeprature Approaching high"
+    else:
+        temp_message="Temperature is OK"
+    return temp_message
+   
+#soc_tolerance
+def check_soc_tolerance(soc):
+    if(soc<20 and soc>80):
+        soc_message="State of Charge out of range"
+    elif (soc>=20 and soc<=(20+0.05*80)): 
+        soc_message= "Warning:SOC Approaching discharge"
+    elif(soc>=(80-0.05*80) and soc<=80):
+        soc_message="Warning:SOC Approaching charge-peak"
+    else:
+        soc_message="State of Charge is OK"
+    return soc_message
+    
+#chargerate_tolerance
+def check_chargerate_tolerance(chargeRate):
+    if(chargeRate>0.8):
+        chargerate_message="Warning:Charge Rate out of range"
+    elif(chargeRate>=(0.8-0.05*0.8) and chargeRate<=0.8):
+        chargerate_message= "Warning:Charge rate Approaching peak"
+    else:
+        chargerate_message="Charge Rate is OK"
+    return chargerate_message
+        
+
 
 #Temperature verification
 def Temperature_Ok(temperature):
