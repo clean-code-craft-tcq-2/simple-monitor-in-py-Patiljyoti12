eedfe6ab97933ad93c38a1f8e7f2b19b7846c00c
Patiljyoti12 <59950788+Patiljyoti12@users.noreply.github.com>
2022-02-14 16:54:09 +0530

    Update check_limits.py

--- check_limits.py
+++ check_limits.py
@@ -37,15 +37,13 @@ def check_chargerate_tolerance(chargeRate):
 #Temperature verification
 def Temperature_Ok(temperature):
     if(temperature<0 or temperature>45):
-        print_warning("Temperature")
-        return False
+       return False
     else:
         return True
         
 #SOC verification         
 def SOC_Ok(soc):
     if(soc<20 or soc>80):
-        print_warning("State of Charge")
         return False
     else:
         return True
@@ -53,7 +51,6 @@ def SOC_Ok(soc):
 # Chargerate verification        
 def Chargerate_Ok(chargeRate):
     if(chargeRate >0.8):
-        print_warning("Charge Rate")
         return False
     else:
         return True
