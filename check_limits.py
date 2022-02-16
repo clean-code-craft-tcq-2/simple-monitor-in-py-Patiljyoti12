ea8140d3bea51cdd779b0e894d3e8f952161dc85
Patiljyoti12 <59950788+Patiljyoti12@users.noreply.github.com>
2022-02-16 10:09:41 +0530

    Update check_limits.py

--- check_limits.py
+++ check_limits.py
@@ -1,8 +1,6 @@
 #Temperature_tolerance
 def check_temp_tolerance(temperature):
-    if (temperature<0 and  temperature>45):
-        temp_message="Temperature out of range"
-    elif (temperature>=0 and temperature<=(0+0.05*45)):
+    if(temperature>=0 and temperature<=(0+0.05*45)):
         temp_message="Warning:Temperature Approaching low"
     elif (temperature >=(45-0.05*45)and temperature<=45):
         temp_message="Warning:Temeprature Approaching high"
@@ -12,9 +10,7 @@ def check_temp_tolerance(temperature):
    
 #soc_tolerance
 def check_soc_tolerance(soc):
-    if(soc<20 and soc>80):
-        soc_message="State of Charge out of range"
-    elif (soc>=20 and soc<=(20+0.05*80)): 
+    if (soc>=20 and soc<=(20+0.05*80)): 
        soc_message="Warning:SOC Approaching discharge"
     elif(soc>=(80-0.05*80) and soc<=80):
        soc_message="Warning:SOC Approaching charge-peak"
@@ -24,13 +20,10 @@ def check_soc_tolerance(soc):
     
 #chargerate_tolerance
 def check_chargerate_tolerance(chargeRate):
-    if(chargeRate>0.8):
-       chargerate_message="Warning:Charge Rate out of range"   
-    elif(chargeRate>=(0.8-0.05*0.8) and chargeRate<=0.8):
+    if(chargeRate>=(0.8-0.05*0.8) and chargeRate<=0.8):
         chargerate_message= "Warning:Charge rate Approaching peak"
     else:
        chargerate_message="Charge Rate is OK"
-        
     return chargerate_message
 
 
