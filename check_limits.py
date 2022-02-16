c638b0ce71ad4c0a133cb77552854d5d8994a3ab
Patiljyoti12 <59950788+Patiljyoti12@users.noreply.github.com>
2022-02-16 11:01:48 +0530

    Update check_limits.py

--- check_limits.py
+++ check_limits.py
@@ -1,26 +1,26 @@
 #Temperature_tolerance
 def check_temp_tolerance(temperature):
     if(temperature>=0 and temperature<=(0+0.05*45)):
-        temp_message="Warning:Temperature Approaching low"
+        print("Warning:Temperature Approaching low")
     elif (temperature >=(45-0.05*45)and temperature<=45):
-        temp_message="Warning:Temeprature Approaching high"
-    return temp_message
+        print("Warning:Temeprature Approaching high")
+    
    
 #soc_tolerance
 def check_soc_tolerance(soc):
     if (soc>=20 and soc<=(20+0.05*80)): 
-       soc_message="Warning:SOC Approaching discharge"
+       print("Warning:SOC Approaching discharge")
     elif(soc>=(80-0.05*80) and soc<=80):
-       soc_message="Warning:SOC Approaching charge-peak"
-    return soc_message
+       print("Warning:SOC Approaching charge-peak")
+  
     
 #chargerate_tolerance
 def check_chargerate_tolerance(chargeRate):
     if(chargeRate>=(0.8-0.05*0.8) and chargeRate<=0.8):
-        chargerate_message= "Warning:Charge rate Approaching peak"
+       print("Warning:Charge rate Approaching peak")
     else:
-       chargerate_message="Charge Rate is OK"
-    return chargerate_message
+       print("Charge Rate is OK")
+   
 
 
 
